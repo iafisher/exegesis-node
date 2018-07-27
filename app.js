fa9cbd7500cb9a75c52c4166dc43bb7c1afe8615
Ian Fisher <iafisher@protonmail.com>
2018-07-26 22:45:18 -0400

    Rename everything, again

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-var mainPageRouter = require('./routes/mainpage');
+var indexRouter = require('./routes/index');
+var snippetRouter = require('./routes/snippet');
 var secrets = require('./secrets');
 
 var snippetModel = require('./models/snippet.js');
@@ -34,7 +35,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', mainPageRouter);
+app.use('/', indexRouter);
+app.use('/snippet', snippetRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('mainpage');
+    res.render('index');
 });
 
 module.exports = router;
